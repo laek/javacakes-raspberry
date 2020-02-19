b7221e0e0badcc5977a802b5dffce0bb33b37631
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-02-19 15:47:31 +0000

    adding Main and TakePicture classes

--- src/main/java/com/makers/javacakesraspberry/Main.java
+++ src/main/java/com/makers/javacakesraspberry/Main.java
@@ -5,10 +5,33 @@
  */
 package com.makers.javacakesraspberry;
 
+import java.util.Scanner;
+
 /**
  *
  * @author laura
  */
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        while(true) {
+
+            System.out.println("Enter Input:");
+
+            String userInput = input.nextLine();
+
+            if(userInput.equals("a")) {
+
+                TakePicture picture = new TakePicture();
+
+            } else {
+
+                System.out.println("Try again...");
+
+            }
+        }
+    }
 }

d5eaa82c43ca7f1df1a23cf57f2c4d24be80ef46
Laura Kytonen <laura.kytonen@gmail.com>
2020-02-19 15:35:23 +0000

    email sending functionality

--- src/main/java/com/makers/javacakesraspberry/Main.java
+++ src/main/java/com/makers/javacakesraspberry/Main.java
@@ -11,4 +11,32 @@ package com.makers.javacakesraspberry;
  */
 public class Main {
     
+    public static void main(String[] args) {
+        // SMTP server information
+        String host = "smtp.gmail.com";
+        String port = "587";
+        String mailFrom = "javacakes101@gmail.com";
+        String password = "makers2020";
+ 
+        // outgoing message information
+        String mailTo = "laura.kytonen@gmail.com";
+        String subject = "Hello from javacakes with an attachment";
+        String message = "Hi guy, Hope you have a lot of javacakes. Duke.";
+        
+        // attachments
+        String[] attachFiles = new String[3];
+        attachFiles[0] = "/home/pi/cam.jpg";
+ 
+        EmailSender mailer = new EmailSender();
+ 
+        try {
+            mailer.sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+                    subject, message, attachFiles);
+            System.out.println("Email sent.");
+        } catch (Exception ex) {
+            System.out.println("Failed to sent email.");
+            ex.printStackTrace();
+        }
+    }
+    
 }

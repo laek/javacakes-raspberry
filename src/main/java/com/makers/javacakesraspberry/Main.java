73b6770029f00650be4cc211cc7ebc6abd7bf799
Masha N <marianetchaeva@hotmail.com>
2020-02-20 11:25:44 +0000

    email sends with attachment from rasbpi

--- src/main/java/com/makers/javacakesraspberry/Main.java
+++ src/main/java/com/makers/javacakesraspberry/Main.java
@@ -4,6 +4,21 @@
  * and open the template in the editor.
  */
 package com.makers.javacakesraspberry;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.MimeBodyPart;
 
 /**
  *
@@ -19,12 +34,12 @@ public class Main {
         String password = "makers2020";
  
         // outgoing message information
-        String mailTo = "laura.kytonen@gmail.com";
+        String mailTo = "marianetchaeva@gmail.com";
         String subject = "Hello from javacakes with an attachment";
         String message = "Hi guy, Hope you have a lot of javacakes. Duke.";
         
         // attachments
-        String[] attachFiles = new String[3];
+        String[] attachFiles = new String[1];
         attachFiles[0] = "/home/pi/cam.jpg";
  
         EmailSender mailer = new EmailSender();
@@ -32,9 +47,12 @@ public class Main {
         try {
             mailer.sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
                     subject, message, attachFiles);
-            System.out.println("Email sent.");
+            System.out.println("Email sent with attachment v.1.");
         } catch (Exception ex) {
-            System.out.println("Failed to sent email.");
+//            mailer.sendPlainTextEmail(host, port, mailFrom, password, mailTo,
+//                subject, message);
+//            System.out.println("Email sent without attachment.");
+            System.out.println("Failed to sent email with attachment.");
             ex.printStackTrace();
         }
     }

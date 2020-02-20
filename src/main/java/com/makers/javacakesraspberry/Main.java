2147fb5d0d55354401a8cdad8a448a4107e612ea
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-02-20 12:30:28 +0000

    COMPLETE MVP

--- src/main/java/com/makers/javacakesraspberry/Main.java
+++ src/main/java/com/makers/javacakesraspberry/Main.java
@@ -1,23 +1,10 @@
 package com.makers.javacakesraspberry;
-import java.io.IOException;
-import java.util.Date;
-import java.util.Properties;
-import javax.mail.Authenticator;
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Multipart;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.Transport;
-import javax.mail.internet.AddressException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
-import javax.mail.internet.MimeBodyPart;
 
 import com.pi4j.io.gpio.*;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -27,6 +14,23 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Running...");
+
+        // SMTP server information
+        String host = "smtp.gmail.com";
+        String port = "587";
+        String mailFrom = "javacakes101@gmail.com";
+        String password = "makers2020";
+
+        // outgoing message information
+        String mailTo = "javacakes101@gmail.com";
+        String subject = "Hello from javacakes with an attachment";
+        String message = "Hi guy, Hope you have a lot of javacakes. Duke.";
+
+        // attachments
+        String[] attachFiles = new String[1];
+        attachFiles[0] = "/home/pi/javacakes/images/pic.jpg";
+
+        EmailSender mailer = new EmailSender();
 
         // create gpio controller
         final GpioController gpio = GpioFactory.getInstance();
@@ -45,6 +49,22 @@ public class Main {
 
                 if (buttonPressed) {
                     TakePicture picture = new TakePicture();
+
+                    try {
+                        TimeUnit.SECONDS.sleep(10);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        mailer.sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
+                                subject, message, attachFiles);
+                        System.out.println("Email sent with attachment.");
+                    } catch (Exception ex) {
+                        System.out.println("Failed to sent email with attachment.");
+                        ex.printStackTrace();
+                    }
+
                 }
             }
         });
@@ -55,31 +75,7 @@ public class Main {
         while(true) {
             Thread.sleep(500);
         }
-        // SMTP server information
-        String host = "smtp.gmail.com";
-        String port = "587";
-        String mailFrom = "javacakes101@gmail.com";
-        String password = "makers2020";
- 
-        // outgoing message information
-        String mailTo = "<ADD YOUR EMAIL ADDRESS>";
-        String subject = "Hello from javacakes with an attachment";
-        String message = "Hi guy, Hope you have a lot of javacakes. Duke.";
-        
-        // attachments
-        String[] attachFiles = new String[1];
-        attachFiles[0] = "/home/pi/cam.jpg";
- 
-        EmailSender mailer = new EmailSender();
- 
-        try {
-            mailer.sendEmailWithAttachments(host, port, mailFrom, password, mailTo,
-                    subject, message, attachFiles);
-            System.out.println("Email sent with attachment.");
-        } catch (Exception ex) {
-            System.out.println("Failed to sent email with attachment.");
-            ex.printStackTrace();
-        }
+
     }
     
 }
